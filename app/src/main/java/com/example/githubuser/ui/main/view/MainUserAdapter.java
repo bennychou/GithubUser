9f22182238c1cc30978233928da68129f56529a0
bennychou <bennychou7573@hotmail.com>
2018-08-13 15:56:28 +0900

    Update ui of user list

--- app/src/main/java/com/example/githubuser/ui/main/view/MainUserAdapter.java
+++ app/src/main/java/com/example/githubuser/ui/main/view/MainUserAdapter.java
@@ -2,6 +2,7 @@ package com.example.githubuser.ui.main.view;
 
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,6 +11,7 @@ import android.widget.TextView;
 
 import com.example.githubuser.R;
 import com.example.githubuser.data.entity.User;
+import com.example.githubuser.ui.CircleTransform;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
@@ -17,9 +19,12 @@ import java.util.List;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 
-public class MainUserAdapter extends RecyclerView.Adapter<MainUserAdapter.ViewHolder> {
+public class MainUserAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
     private List<User> users;
     private UserListListener userListListener;
+    private static final int TYPE_HEADER = 0;
+    private static final int TYPE_USER = 1;
+    private static final int headerCount = 1;
     public MainUserAdapter(List<User> users) {
         this.users = users;
     }
@@ -28,24 +33,41 @@ public class MainUserAdapter extends RecyclerView.Adapter<MainUserAdapter.ViewHo
         this.userListListener = listener;
     }
 
+    @Override
+    public int getItemViewType(int position) {
+        if (position == 0)
+            return TYPE_HEADER;
+        else
+            return TYPE_USER;
+    }
+
     @NonNull
     @Override
-    public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-        View view = LayoutInflater.from(viewGroup.getContext())
-                .inflate(R.layout.item_user, viewGroup, false);
-        return new ViewHolder(view);
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType) {
+        if (viewType == TYPE_HEADER) {
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.item_header, viewGroup, false);
+            return new HeaderViewHolder(view);
+        } else {
+            View view = LayoutInflater.from(viewGroup.getContext())
+                    .inflate(R.layout.item_user, viewGroup, false);
+            return new ViewHolder(view);
+        }
     }
 
     @Override
-    public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
-        final User user = users.get(i);
-        Picasso.get().load(user.getAvatarUrl()).into(viewHolder.icon);
-        viewHolder.name.setText(user.getName());
-        viewHolder.type.setText(user.getType());
-        viewHolder.itemView.setOnClickListener(v -> {
-            if (userListListener != null)
-                userListListener.onItemClicked(users.get(i));
-        });
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder viewHolder, int i) {
+        if (viewHolder instanceof ViewHolder) {
+            final User user = users.get(i - headerCount);
+            Picasso.get().load(user.getAvatarUrl()).transform(new CircleTransform()).into(((ViewHolder) viewHolder).icon);
+            ((ViewHolder) viewHolder).name.setText(user.getName());
+//            ((ViewHolder) viewHolder).type.setText(user.getType());
+            ((ViewHolder) viewHolder).admin.setVisibility(user.isSiteAdmin() ? View.VISIBLE : View.GONE);
+            ((ViewHolder) viewHolder).itemView.setOnClickListener(v -> {
+                if (userListListener != null)
+                    userListListener.onItemClicked(users.get(i));
+            });
+        }
     }
 
     void updateList(@NonNull final List<User> users) {
@@ -59,7 +81,7 @@ public class MainUserAdapter extends RecyclerView.Adapter<MainUserAdapter.ViewHo
 
     @Override
     public int getItemCount() {
-        return users.size();
+        return headerCount + users.size();
     }
 
     class ViewHolder extends RecyclerView.ViewHolder {
@@ -74,7 +96,23 @@ public class MainUserAdapter extends RecyclerView.Adapter<MainUserAdapter.ViewHo
         @BindView(R.id.type)
         TextView type;
 
+        @BindView(R.id.admin)
+        TextView admin;
+
         public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            ButterKnife.bind(this, itemView);
+            this.itemView = itemView;
+        }
+    }
+
+    class HeaderViewHolder extends RecyclerView.ViewHolder {
+        View itemView;
+
+        @BindView(R.id.header)
+        TextView header;
+
+        public HeaderViewHolder(@NonNull View itemView) {
             super(itemView);
             ButterKnife.bind(this, itemView);
             this.itemView = itemView;
